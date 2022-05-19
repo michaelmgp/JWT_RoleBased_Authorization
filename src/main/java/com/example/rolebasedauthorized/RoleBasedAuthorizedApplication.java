d5c7dab368f087f7edb54b7470b2544a09f5b7f1
Michael MGP Simatupang <mgpmichael@gmail.com>
2022-05-19 21:34:39 +0700

    first commit

---
+++ src/main/java/com/example/rolebasedauthorized/RoleBasedAuthorizedApplication.java
@@ -0,0 +1,13 @@
+package com.example.rolebasedauthorized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedAuthorizedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoleBasedAuthorizedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rolebasedauthorized/RoleBasedAuthorizedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rolebasedauthorized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedAuthorizedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
